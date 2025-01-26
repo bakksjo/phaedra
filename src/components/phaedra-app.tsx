55cbd91b5e30b60d86977262a01f1bdc072b3b29
Oyvind Bakksjo <obakksjo@microsoft.com>
2025-01-26 20:11:24 +0100

    Make outgoing call to fetch TODOs. Display TODOs.

--- src/components/phaedra-app.tsx
+++ src/components/phaedra-app.tsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-export const PhaedraApp: React.FC = () => {
-  return <h1>Hello from React!</h1>;
+export const PhaedraApp = () => {
+
+  interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+
+  interface IPhaedraAppState {
+    todos: Todo[];
+    loading: boolean;
+  }
+
+  const initialState: IPhaedraAppState = {
+    todos: [],
+    loading: true,
+  };
+
+  const [state, setState] = useState<IPhaedraAppState>(initialState);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json() as Promise<Todo[]>)
+      .then(todos => setState((prevState) =>({ ...prevState, todos: todos.slice(0, 10), loading: false })))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+
+      {state.loading && (
+        <h2>Loading...</h2>
+      )}
+
+      { state.todos.length == 0 && !state.loading
+        ? ( <h2>No todos found</h2> )
+        : (
+          <ul>
+            {state.todos.map(todo => (
+              <li key={todo.id}>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </li>
+            ))}
+          </ul>
+        )
+      }
+    </div>
+  );
 }
