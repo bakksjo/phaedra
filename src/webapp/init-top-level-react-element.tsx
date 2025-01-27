449304ad55aa330cdac654fd152b26793c752987
Oyvind Bakksjo <obakksjo@microsoft.com>
2025-01-27 13:51:02 +0100

    Renamed React file (PascalCase).

--- src/webapp/init-top-level-react-element.tsx
+++ src/webapp/init-top-level-react-element.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { PhaedraApp } from '../components/phaedra-app';
+import { PhaedraApp } from '../components/PhaedraApp';
 
 const domNode = document.getElementById('phaedra-app');
 const root = createRoot(domNode);
