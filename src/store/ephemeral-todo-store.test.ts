41949cd05db3bab6074a2f79fead4b90dd513185
Oyvind Bakksjo <obakksjo@microsoft.com>
2025-02-03 18:26:34 +0100

    Add more tests for store.

--- src/store/ephemeral-todo-store.test.ts
+++ src/store/ephemeral-todo-store.test.ts
@@ -6,13 +6,20 @@ import { EphemeralTodoStore } from './ephemeral-todo-store';
   // Unfortunately, Jest matchers don't play well with discriminated unions so we need this :-(
 const unreachableButRequiredForNarrowing = () => { throw new Error("Should never happen - present for type narrowing only"); };
 
-const expectTodosContain = (storedTodos: StoredTodoItem[] | undefined, ...expectedTodos: TodoItemData[]) => {
+const expectTodosToContain = (storedTodos: StoredTodoItem[] | undefined, ...expectedTodos: TodoItemData[]) => {
   expect(storedTodos).toBeDefined();
   if (!storedTodos) throw unreachableButRequiredForNarrowing();
 
   const todosDataOnly = storedTodos.map(todo => todo.data);
   expectedTodos.forEach(todo => expect(todosDataOnly).toContainEqual(todo));
 }
+
+const VALIDATION_ERROR = "Can only append to the title";
+const titleAppendOnlyValidator = (currentItem: TodoItemData, proposedItem: TodoItemData): string | undefined => {
+  if (!proposedItem.title.startsWith(currentItem.title)) {
+    return VALIDATION_ERROR;
+  }
+};
 
 describe('EphemeralTodoStore', () => {
   const sut = new EphemeralTodoStore();
@@ -61,7 +68,7 @@ describe('EphemeralTodoStore', () => {
     const todos = sut.list(listName);
 
     expect(todos).toHaveLength(2);
-    expectTodosContain(todos, todo, secondTodo);
+    expectTodosToContain(todos, todo, secondTodo);
   });
 
   test('updates are reflected in the store', () => {
@@ -83,7 +90,7 @@ describe('EphemeralTodoStore', () => {
 
     const todos = sut.list(listName);
     expect(todos).toHaveLength(1);
-    expectTodosContain(todos, updatedTodo);
+    expectTodosToContain(todos, updatedTodo);
   });
 
   test('deleted items are removed from store', () => {
@@ -113,11 +120,11 @@ describe('EphemeralTodoStore', () => {
 
     const firstList = sut.list(listName);
     expect(firstList).toHaveLength(1);
-    expectTodosContain(firstList, todo);
+    expectTodosToContain(firstList, todo);
 
     const secondList = sut.list(anotherListName);
     expect(secondList).toHaveLength(1);
-    expectTodosContain(secondList, anotherTodo);
+    expectTodosToContain(secondList, anotherTodo);
   });
   
   test('returns undefined for non-existent TODO item', () => {
@@ -140,7 +147,100 @@ describe('EphemeralTodoStore', () => {
     expect(lists).toContain(anotherListName);
   });
 
-  // TODO: Need a lot of new tests (revisions, conflicts, etc.)
+  test('returns conflict result on version conflict during update', () => {
+    const res1 = sut.create(listName, todo);
+    if (res1.result !== 'created') throw unreachableButRequiredForNarrowing();
+
+    const updatedTodo: TodoItemData = { ...todo, title: 'Updated TODO' };
+    const wrongRevision = res1.todo.meta.revision - 1;
+
+    const res2 = sut.update(listName, res1.todo.meta.id, updatedTodo, wrongRevision);
+    expect(res2.result).toBe('conflict');
+    if (res2.result !== 'conflict') throw unreachableButRequiredForNarrowing();
+    expect(res2.currentItem).toEqual(res1.todo);
+
+    // Update attempt should not have changed any item in store.
+    const todos = sut.list(listName);
+    expect(todos).toHaveLength(1);
+    expectTodosToContain(todos, todo);
+  });
+
+  test('update with validator success is applied', () => {
+    const res1 = sut.create(listName, todo);
+    if (res1.result !== 'created') throw unreachableButRequiredForNarrowing();
+
+    const validUpdate: TodoItemData = { ...todo, title: todo.title + ' suffix' };
+
+    const res2 = sut.update(listName, res1.todo.meta.id, validUpdate, res1.todo.meta.revision, titleAppendOnlyValidator);
+    expect(res2.result).toBe('updated');
+    if (res2.result !== 'updated') throw unreachableButRequiredForNarrowing();
+    
+    const todos = sut.list(listName);
+    expect(todos).toHaveLength(1);
+    expectTodosToContain(todos, validUpdate);
+  });
+
+  test('update with validator failure is not applied', () => {
+    const res1 = sut.create(listName, todo);
+    if (res1.result !== 'created') throw unreachableButRequiredForNarrowing();
+
+    const invalidUpdate: TodoItemData = { ...todo, title: 'An entirely different title' };
+
+    const res2 = sut.update(listName, res1.todo.meta.id, invalidUpdate, res1.todo.meta.revision, titleAppendOnlyValidator);
+    expect(res2.result).toBe('validation-failure');
+    if (res2.result !== 'validation-failure') throw unreachableButRequiredForNarrowing();
+    expect(res2.validationError).toEqual(VALIDATION_ERROR);
+
+    // Update attempt should not have changed any item in store.
+    const todos = sut.list(listName);
+    expect(todos).toHaveLength(1);
+    expectTodosToContain(todos, todo);
+  });
+
+  test('fails to create TODO in non-existent list', () => {
+    const result = sut.create('non-existent-list', todo);
+    expect(result.result).toBe('not-found');
+  });
+
+  test('can delete a TODO item', () => {
+    const res1 = sut.create(listName, todo);
+    if (res1.result !== 'created') throw unreachableButRequiredForNarrowing();
+
+    const deleteResult = sut.delete(listName, res1.todo.meta.id, res1.todo.meta.revision);
+    expect(deleteResult.result).toBe('deleted');
+
+    const todos = sut.list(listName);
+    expect(todos).toHaveLength(0);
+  });
+
+  test('returns conflict on delete with wrong version', () => {
+    const res1 = sut.create(listName, todo);
+    if (res1.result !== 'created') throw unreachableButRequiredForNarrowing();
+
+    const wrongVersion = res1.todo.meta.revision - 1;
+    const deleteResult = sut.delete(listName, res1.todo.meta.id, wrongVersion);
+    expect(deleteResult.result).toBe('conflict');
+    if (deleteResult.result !== 'conflict') throw unreachableButRequiredForNarrowing();
+    expect(deleteResult.currentItem).toEqual(res1.todo);
+
+    const todos = sut.list(listName);
+    expect(todos).toHaveLength(1);
+    expectTodosToContain(todos, todo);
+  });
+
+  test('fails to delete non-existent TODO item', () => {
+    const deleteResult = sut.delete(listName, 'non-existent-id', 1);
+    expect(deleteResult.result).toBe('not-found');
+    if (deleteResult.result !== 'not-found') throw unreachableButRequiredForNarrowing();
+    expect(deleteResult.missing).toBe('todo');
+  });
+
+  test('fails to delete a TODO item from a non-existent list', () => {
+    const deleteResult = sut.delete('non-existent-list', 'item-id-is-irrelevant', 1);
+    expect(deleteResult.result).toBe('not-found');
+    if (deleteResult.result !== 'not-found') throw unreachableButRequiredForNarrowing();
+    expect(deleteResult.missing).toBe('list');
+  });
 });
 
 describe('EphemeralTodoStore without any lists', () => {
